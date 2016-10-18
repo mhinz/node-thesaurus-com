0ccfeeaeb408ff4d065a1b3e20b60583c688b0b1
Marco Hinz <mh.codebro@gmail.com>
2016-10-19 00:24:27 +0200

    Use var for variable declarations

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var request = require('request');
-var cheerio = require('cheerio');
-
 if (process.argv.length <= 2) {
     var path = require('path');
     console.log('usage: ' + path.basename(process.argv[1]) + ' <query>');
     process.exit(1);
 }
 
+var request = require('request');
+var cheerio = require('cheerio');
+
 var url = 'http://www.thesaurus.com/browse/' +
     encodeURIComponent(process.argv.slice(2).join(' '));
 
 request(url, function(err, resp, body){
     $ = cheerio.load(body, { ignoreWhitespace: true });
-    text     = $('.synonym-description .txt').text();
-    desc     = $('.synonym-description .ttl').text();
-    synonyms = $('div.relevancy-list ul li a span.text').map(function(_i, _element){
+    var text     = $('.synonym-description .txt').text();
+    var desc     = $('.synonym-description .ttl').text();
+    var synonyms = $('div.relevancy-list ul li a span.text').map(function(_i, _element){
         return $(this).text();
     }).get().sort().join(', ');
-    antonyms = $('div.list-holder ul li a span.text').map(function(_i, _element){
+    var antonyms = $('div.list-holder ul li a span.text').map(function(_i, _element){
         return $(this).text();
     }).get().sort().join(', ');
 
