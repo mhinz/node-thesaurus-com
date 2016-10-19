5bc3055a9c482e8f4bde0ebc63ea4ef31787d423
Marco Hinz <mh.codebro@gmail.com>
2016-10-19 14:12:56 +0200

    Refactoring

--- index.js
+++ index.js
@@ -1,63 +1,26 @@
-#!/usr/bin/env node
-
 var request = require('sync-request');
 var cheerio = require('cheerio');
 
-function help_me() {
-    var path = require('path');
-    console.log([
-            'usage: ' + path.basename(process.argv[1]) + ' [-j|-h] <query>',
-            '',
-            'options:',
-            '',
-            '  -j|--json    return JSON',
-            '  -h|--help    show this help',
-            ''
-    ].join('\n'));
-    process.exit(1);
-}
+function search(query) {
+    var url = 'http://www.thesaurus.com/browse/' + encodeURIComponent(query);
+    var res = request('GET', url);
 
-var args = process.argv.slice(2);
-var output_json = 0;
+    $ = cheerio.load(res.getBody(), { ignoreWhitespace: true });
 
-switch (args[0]) {
-    case '-j':
-    case '--json':
-        output_json = 1;
-        args.shift();
-        break;
-    case '-h':
-    case '--help':
-        help_me();
-}
+    var synonyms = $('div.relevancy-list ul li a span.text');
+    var synonyms = synonyms.map(function(_i, _element){
+        return $(this).text();
+    }).get().sort();
 
-if (args.length == 0) {
-    help_me();
-}
+    var antonyms = $('div.list-holder ul li a span.text');
+    var antonyms = antonyms.map(function(_i, _element){
+        return $(this).text();
+    }).get().sort();
 
-var url = 'http://www.thesaurus.com/browse/' + encodeURIComponent(args.join(' '));
-
-var res = request('GET', url)
-$ = cheerio.load(res.getBody(), { ignoreWhitespace: true });
-
-var synonyms = $('div.relevancy-list ul li a span.text');
-var synonyms = synonyms.map(function(_i, _element){
-    return $(this).text();
-}).get().sort();
-
-var antonyms = $('div.list-holder ul li a span.text');
-var antonyms = antonyms.map(function(_i, _element){
-    return $(this).text();
-}).get().sort();
-
-if (output_json) {
-    console.log(JSON.stringify({
+    return {
         synonyms: synonyms,
         antonyms: antonyms
-    }));
-} else {
-    console.log('Synonyms: ' + synonyms.join(', '));
-    if (antonyms.length > 0) {
-        console.log('Antonyms: ' + antonyms.join(', '));
-    }
+    };
 }
+
+exports.search = search
